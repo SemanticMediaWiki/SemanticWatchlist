12dc50261f10423231ba4cc906d90c0f267331ef
Raimond Spekking <raymond@users.mediawiki.org>
2011-05-07 21:26:26 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-05-07 20:56:00 UTC)

--- SemanticWatchlist.i18n.php
+++ SemanticWatchlist.i18n.php
@@ -33,3 +33,44 @@ $messages['en'] = array(
 $messages['qqq'] = array(
 	'semanticwatchlist-desc' => '{{desc}}',
 );
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'semanticwatchlist-desc' => 'Ermöglicht das Festlegen von Gruppen semantischer Attribute für eine oder mehrere Kategorien/ Namensräume in Form semantischer Beobachtungslisten',
+	'right-semanticwatch' => 'Semantische Beobachtungslisten verwenden',
+	'right-semanticwatchgroups' => 'Semantische Beobachtungslisten [[Special:WatchlistConditions|anpassen]]',
+	'special-semanticwatchlist' => 'Semantische Beobachtungslisten',
+	'special-watchlistconditions' => 'Einstellungen zu semantischen Beobachtungslisten',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'semanticwatchlist-desc' => 'Permitte specificar gruppos de proprietates semantic pro un o plus categorias o spatios de nomines, le quales pote alora esser observate pro modificationes',
+	'right-semanticwatch' => 'Usar observatorio semantic',
+	'right-semanticwatchgroups' => '[[Special:WatchlistConditions|Modificar]] le gruppos del observatorio semantic',
+	'special-semanticwatchlist' => 'Observatorio semantic',
+	'special-watchlistconditions' => 'Conditiones del observatorio semantic',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'semanticwatchlist-desc' => 'Овозможува назначување на групи семантички својства за една или повеќе категории/именски простори што можат да се набљудуваат (дали се измениле)',
+	'right-semanticwatch' => 'Користење на семантички список на набљудувања',
+	'right-semanticwatchgroups' => '[[Special:WatchlistConditions|Менување]] на групи од семантички списоци на набљудувања',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'semanticwatchlist-desc' => 'Maakt het mogelijk groepen van semantische eigenschappen aan te geven voor een of meer categorieën of naamruimten, zodat wijzigingen kunnen worden weergegeven',
+	'right-semanticwatch' => 'Semantische volglijst gebruiken',
+	'right-semanticwatchgroups' => 'De semantische volglijstgroepen [[Special:WatchlistConditions|aanpassen]]',
+);
+
