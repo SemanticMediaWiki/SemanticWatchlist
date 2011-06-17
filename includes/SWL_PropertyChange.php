0225fc263935b20bf1463480613b1ac9563cd0c8
Sam Reed <reedy@users.mediawiki.org>
2011-06-17 16:28:18 +0000

    Swap else if for elseif
    Trimming trailing whitespace also
    
    last one! ;)

--- includes/SWL_PropertyChange.php
+++ includes/SWL_PropertyChange.php
@@ -2,12 +2,12 @@
 
 /**
  * Represents a change to a semantic property.
- * 
+ *
  * @since 0.1
- * 
+ *
  * @file SWL_PropertyChange.php
  * @ingroup SemanticWatchlist
- * 
+ *
  * @licence GNU GPL v3 or later
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
@@ -16,27 +16,27 @@ class SWLPropertyChange {
 	const TYPE_INSERT = 0;
 	const TYPE_UPDATE = 1;
 	const TYPE_DELETE = 2;
-	
+
 	/**
 	 * The old value.
-	 * 
+	 *
 	 * @var SMWDataItem or null
-	 */	
+	 */
 	protected $oldValue;
-	
+
 	/**
 	 * The new value.
-	 * 
+	 *
 	 * @var SMWDataItem or null
 	 */
 	protected $newValue;
-	
+
 	/**
 	 * Creates and returns a new SWLPropertyChange instance from a serialization.
-	 * 
+	 *
 	 * @param string|null $oldValue
 	 * @param string|null $newValue
-	 * 
+	 *
 	 * @return SWLPropertyChange
 	 */
 	public static function newFromSerialization( SMWDIProperty $property, $oldValue, $newValue ) {
@@ -48,10 +48,10 @@ class SWLPropertyChange {
 			is_null( $newValue ) ? null : SMWDataItem::newFromSerialization( $diType, $newValue )
 		);
 	}
-	
+
 	/**
 	 * Create a new SWLPropertyChange.
-	 * 
+	 *
 	 * @param SMWDataItem $oldValue
 	 * @param SMWDataItem $newValue
 	 */
@@ -59,53 +59,52 @@ class SWLPropertyChange {
 		$this->oldValue = $oldValue;
 		$this->newValue = $newValue;
 	}
-	
+
 	/**
 	 * Retruns the old value, or null if there is none.
-	 * 
+	 *
 	 * @return SMWDataItem or null
 	 */
 	public function getOldValue() {
 		return $this->oldValue;
 	}
-	
-	
+
+
 	/**
 	 * returns the new value, or null if there is none.
-	 * 
+	 *
 	 * @return SMWDataItem or null
-	 */	
+	 */
 	public function getNewValue() {
 		return $this->newValue;
 	}
-	
+
 	/**
 	 * Returns the type of the change.
-	 * 
+	 *
 	 * @return element of the SWLPropertyChange::TYPE_ enum
-	 */	
+	 */
 	public function getType() {
 		if ( is_null( $this->oldValue ) ) {
 			return self::TYPE_INSERT;
 		}
-		else if ( is_null( $this->newValue ) ) {
+		elseif ( is_null( $this->newValue ) ) {
 			return self::TYPE_DELETE;
 		}
 		else {
 			return self::TYPE_UPDATE;
 		}
 	}
-	
+
 	/**
 	 * Returns a serialized version of the change, suitable to
 	 * do equal comparisions but not to unserialize.
-	 * 
+	 *
 	 * @return string
 	 */
 	public function getSerialization() {
 		return is_null( $this->oldValue ) ? '' : $this->oldValue->getSerialization() . '|' .
 			is_null( $this->newValue ) ? '' : $this->newValue->getSerialization();
 	}
-	
+
 }
-	
