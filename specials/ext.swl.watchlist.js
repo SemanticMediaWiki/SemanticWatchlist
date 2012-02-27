95733da89fef7fad5a676114260c09401603a280
Tim Starling <tstarling@users.mediawiki.org>
2012-02-27 01:02:55 +0000

    Fixed a typo in a boilerplate comment that Jeroen copied to many files, JavasSript -> JavaScript

--- specials/ext.swl.watchlist.js
+++ specials/ext.swl.watchlist.js
@@ -1,5 +1,5 @@
 /**
- * JavasSript for Special:SemanticWatchlist in the Semantic Watchlist extension.
+ * JavaScript for Special:SemanticWatchlist in the Semantic Watchlist extension.
  * @see http://www.mediawiki.org/wiki/Extension:Semantic_Watchlist
  * 
  * @licence GNU GPL v3 or later
--- specials/ext.swl.watchlistconditions.js
+++ specials/ext.swl.watchlistconditions.js
@@ -1,5 +1,5 @@
 /**
- * JavasSript for Special:WatchlistConditions in the Semantic Watchlist extension.
+ * JavaScript for Special:WatchlistConditions in the Semantic Watchlist extension.
  * @see http://www.mediawiki.org/wiki/Extension:Semantic_Watchlist
  * 
  * @licence GNU GPL v3 or later
