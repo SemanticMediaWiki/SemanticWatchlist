cd316b7277707710fbb3f21fff96099143207535
nischayn22 <nischayn22@gmail.com>
2012-11-09 15:58:26 +0530

    Making saving message Strong
    Change-Id: I51fa7faf46de30ee8a4833ecc05c480b423c8ab6

--- specials/SpecialWatchlistConditions.php
+++ specials/SpecialWatchlistConditions.php
@@ -2,38 +2,38 @@
 
 /**
  * Interface to modify the semantic watchlist groups.
- * 
+ *
  * @since 0.1
- * 
+ *
  * @file SpecialWatchlistConditions.php
  * @ingroup SemanticWatchlist
- * 
+ *
  * @licence GNU GPL v3 or later
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 class SpecialWatchlistConditions extends SpecialPage {
-	
+
 	/**
 	 * Constructor.
-	 * 
+	 *
 	 * @since 0.1
 	 */
 	public function __construct() {
 		parent::__construct( 'WatchlistConditions', 'semanticwatchgroups' );
 	}
-	
+
 	/**
 	 * @see SpecialPage::getDescription
-	 * 
+	 *
 	 * @since 0.1
 	 */
 	public function getDescription() {
 		return wfMsg( 'special-' . strtolower( $this->getName() ) );
 	}
-	
+
 	/**
 	 * Sets headers - this should be called from the execute() method of all derived classes!
-	 * 
+	 *
 	 * @since 0.1
 	 */
 	public function setHeaders() {
@@ -42,43 +42,42 @@ class SpecialWatchlistConditions extends SpecialPage {
 		$wgOut->setRobotPolicy( 'noindex,nofollow' );
 		$wgOut->setPageTitle( $this->getDescription() );
 	}
-	
+
 	/**
 	 * Main method.
-	 * 
+	 *
 	 * @since 0.1
-	 * 
+	 *
 	 * @param string $arg
 	 */
 	public function execute( $arg ) {
 		global $wgOut, $wgUser, $wgRequest;
-		
+
 		$this->setHeaders();
 		$this->outputHeader();
-		
+
 		// If the user is authorized, display the page, if not, show an error.
 		if ( !$this->userCanExecute( $wgUser ) ) {
 			$this->displayRestrictionError();
 			return;
 		}
-		
+
 		$wgOut->addHTML(
-			'<p class="saveMessage" style=" display:none;width: 100px; alight:center;text-align:center; background: #f9f9aa; border: 1px solid #dd9">' .
-			wfMsg( 'swl-group-saved' ) . '</p>'
+			'<strong><p class="saveMessage" style=" display:none;width: 100px; alight:center;text-align:center; background: #f9f9aa; border: 1px solid #dd9">' . wfMsg( 'swl-group-saved' ) . '</p></strong>'
 		);
 
 		$groupsHtml = array();
-		
+
 		foreach ( SWLGroups::getAll() as $group ) {
 			$groupsHtml[] = $this->getGroupHtml( $group );
 		}
-		
+
 		$wgOut->addHTML(
 			'<div id="swl-groups">' .
 				implode( '', $groupsHtml ) .
 			'</div>'
 		);
-		
+
 		$wgOut->addHTML( Html::element(
 			'input',
 			array(
@@ -87,7 +86,7 @@ class SpecialWatchlistConditions extends SpecialPage {
 				'id' => 'swl-add-group-button'
 			)
 		) );
-		
+
 		$wgOut->addHTML( Html::element(
 			'input',
 			array(
@@ -96,26 +95,26 @@ class SpecialWatchlistConditions extends SpecialPage {
 				'id' => 'swl-save-all'
 			)
 		) );
-		
+
 		$wgOut->addModules( 'ext.swl.watchlistconditions' );
 	}
-	
+
 	/**
 	 * Creates and returns the HTML for a single watchlist group.
-	 * 
+	 *
 	 * @since 0.1
-	 * 
+	 *
 	 * @param SWLGroup $group
-	 * 
+	 *
 	 * @return string
 	 */
 	protected function getGroupHtml( SWLGroup $group ) {
 		$namespaces = $group->getNamespaces();
-		
+
 		foreach ( $namespaces as &$ns ) {
 			$ns = $ns == 0 ? 'Main' : MWNamespace::getCanonicalName( $ns );
 		}
-		
+
 		return Html::rawElement(
 			'fieldset',
 			array(
@@ -136,5 +135,5 @@ class SpecialWatchlistConditions extends SpecialPage {
 			)
 		);
 	}
-	
+
 }
