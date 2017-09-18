a9de7296988e577a593cfb467953036d0ab772a0
Jeroen De Dauw <jeroendedauw@gmail.com>
2017-09-19 00:08:30 +0200

    Update CI code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,21 @@
 <?php
 
-if ( php_sapi_name() !== 'cli' ) {
+if ( defined( 'MEDIAWIKI' ) ) {
+	return;
+}
+
+if ( PHP_SAPI !== 'cli' ) {
 	die( 'Not an entry point' );
 }
 
-if ( !is_readable( $autoloaderClassPath = __DIR__ . '/../../SemanticMediaWiki/tests/autoloader.php' ) ) {
-	die( 'The SemanticMediaWiki test autoloader is not available' );
+error_reporting( -1 );
+ini_set( 'display_errors', 1 );
+
+if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
+	die( 'You need to install this package with Composer before you can run the tests' );
 }
 
-print sprintf( "\n%-20s%s\n", "Semantic Watchlist: ", SWL_VERSION );
+$autoloader = require __DIR__ . '/../vendor/autoload.php';
 
-$autoloader = require $autoloaderClassPath;
 $autoloader->addPsr4( 'SWL\\Tests\\', __DIR__ . '/phpunit/Unit' );
 $autoloader->addPsr4( 'SWL\\Tests\\Integration\\', __DIR__ . '/phpunit/Integration' );
