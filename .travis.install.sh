6808e7216312a08cbc90fcaf27d2348c5e68738b
Jeroen De Dauw <jeroendedauw@gmail.com>
2020-03-30 21:26:16 +0200

    Update .travis.install.sh

--- .travis.install.sh
+++ .travis.install.sh
@@ -6,7 +6,7 @@ originalDirectory=$(pwd)
 
 cd ..
 
-wget https://github.com/wikimedia/mediawiki-core/archive/$MW.tar.gz
+wget https://github.com/wikimedia/mediawiki/archive/$MW.tar.gz
 tar -zxf $MW.tar.gz
 mv mediawiki-$MW phase3
 
@@ -30,15 +30,39 @@ cp -r $originalDirectory SemanticWatchlist
 cd SemanticWatchlist
 composer install --prefer-source
 
-[[ ! -z $SMW ]] && composer require "mediawiki/semantic-media-wiki=$SMW" --prefer-source
-
 cd ../..
 
+if [ ! -z $SMW ]
+then
+	composer require "mediawiki/semantic-media-wiki=$SMW" --prefer-source
+fi
+
+cat <<EOT >> composer.local.json
+{
+	"extra": {
+		"merge-plugin": {
+			"merge-dev": true,
+			"include": [
+				"extensions/*/composer.json"
+			]
+		}
+	}
+}
+EOT
+
+composer install --prefer-source
+
 echo 'require_once( __DIR__ . "/extensions/SemanticWatchlist/SemanticWatchlist.php" );' >> LocalSettings.php
+
+if [ ! -z $SMW ]
+then
+	echo 'wfLoadExtension( "SemanticMediaWiki" );' >> LocalSettings.php
+fi
 
 echo 'error_reporting(E_ALL| E_STRICT);' >> LocalSettings.php
 echo 'ini_set("display_errors", 1);' >> LocalSettings.php
 echo '$wgShowExceptionDetails = true;' >> LocalSettings.php
+echo '$wgShowDBErrorBacktrace = true;' >> LocalSettings.php
 echo '$wgDevelopmentWarnings = true;' >> LocalSettings.php
 echo "putenv( 'MW_INSTALL_PATH=$(pwd)' );" >> LocalSettings.php
 
